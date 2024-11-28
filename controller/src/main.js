30f3875fe5323979b033a37271bd3854ffda7d7a
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-11-28 22:35:13 +0800

    Add template websocket server

--- controller/src/main.js
+++ controller/src/main.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { WebSocketServer } from 'ws';
 
 const app = express();
 const port = 3000;
@@ -9,4 +10,14 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+});
+
+const wss = new WebSocketServer({ port: 3001 });
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.send('Welcome to the WebSocket server!');
 });
