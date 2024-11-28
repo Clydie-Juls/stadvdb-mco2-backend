d67b253e58f6f635be1f67e7e8085b97c417fa5a
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-11-28 22:27:48 +0800

    Add controller libs+boilerplate

---
+++ controller/eslint.config.mjs
@@ -0,0 +1,10 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
