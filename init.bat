c335b32672ed7acf22336a26cc63ee36bf4a69c7
clydie-juls <clyde_marindo@dlsu.edu.ph>
2024-11-29 12:24:13 +0800

    delete porting issue in

--- init.bat
+++ init.bat
@@ -1,3 +1,3 @@
-docker compose --env-file central.env -p central up -d
-docker compose --env-file old.env -p old up -d
-docker compose --env-file new.env -p new up -d
+docker compose --env-file central.env -p central up -d --build
+docker compose --env-file old.env -p old up -d --build
+docker compose --env-file new.env -p new up -d --build
