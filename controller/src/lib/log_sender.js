7c2d328e94899f5c6d47cb57bb3bddfccc68090b
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-11-29 18:48:15 +0800

    Add initial controller code

---
+++ controller/src/lib/log_sender.js
@@ -0,0 +1,51 @@
+import { getPeersWS } from './ws_server';
+
+export function sendLogToOthers(logData, wsURL) {
+  const peers = getPeersWS();
+
+  for (const peer of peers) {
+    pollSend(peer, wsURL, {
+      sender: wsURL,
+      log: logData,
+    });
+  }
+
+  console.log('Sent log to all other nodes.');
+}
+
+function pollSend(
+  peer,
+  wsURL,
+  logData,
+  maxRetries = 30,
+  pollingInterval = 2000,
+) {
+  let retryCount = 1;
+
+  // Send the initial message immediately
+  peer.send({
+    sender: wsURL,
+    log: logData,
+  });
+
+  peer.on('error', error => {
+    console.warn('WebSocket error:', error);
+
+    if (retryCount < maxRetries) {
+      retryCount++;
+      console.warn(`Retrying (${retryCount}/${maxRetries})`);
+
+      const delayedSend = () => {
+        peer.send({
+          sender: wsURL,
+          log: logData,
+        });
+        setTimeout(delayedSend, pollingInterval);
+      };
+
+      setTimeout(delayedSend, pollingInterval);
+    } else {
+      console.error('Max retries reached. Giving up.');
+    }
+  });
+}
---
+++ controller/src/lib/ws_server.js
@@ -0,0 +1,49 @@
+import { getEnv } from '../util';
+import { handleMessage } from './message_handler';
+import { WebSocketServer } from 'ws';
+
+let wss;
+
+export function initWSServer(port) {
+  wss = new WebSocketServer({ port });
+
+  wss.once('listening', () => {
+    console.log('Listening on port %d', port);
+  });
+
+  wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+      const { name, ...args } = data;
+      console.log('Received WS message: %s %s', name, JSON.stringify(args));
+
+      handleMessage(name, args);
+    });
+  });
+}
+
+export function getPeersWS() {
+  const peers = [];
+  const peersURL = getEnv('PEER_URLS').split(',');
+
+  for (const peerURL of peersURL) {
+    const ws = new WebSocket(peerURL);
+    peers.push(ws);
+  }
+
+  return peers;
+}
+
+export function sendWSMessage(sender, name, values = {}) {
+  const ws = new WebSocket(sender);
+
+  ws.send(
+    JSON.stringify({
+      name,
+      sender: `${getEnv('CONTROLLER_URL')}/${getEnv('CONTROLLER_PORT')}`,
+      ...values,
+    }),
+  );
+  ws.close();
+}
