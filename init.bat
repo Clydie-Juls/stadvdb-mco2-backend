3946bec095892f7c07a26adae7d669c109d64add
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-11-29 08:02:08 +0800

    complete official docker compose

--- init.bat
+++ init.bat
@@ -1,3 +1,3 @@
-docker compose --env-file central.env -f sql-only-docker-compose.yaml -p central up -d
-docker compose --env-file old.env -f sql-only-docker-compose.yaml -p old up -d
-docker compose --env-file new.env -f sql-only-docker-compose.yaml -p new up -d
+docker compose --env-file central.env -p central up -d
+docker compose --env-file old.env -p old up -d
+docker compose --env-file new.env -p new up -d
