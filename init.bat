7111255595eafc59d5685c9ce4c18bb55d301b09
clydie-juls <clyde_marindo@dlsu.edu.ph>
2024-11-30 10:49:43 +0800

    expose network to other isolated controller nodes

--- init.bat
+++ init.bat
@@ -1,3 +1,4 @@
+docker network create shared-network
 docker compose --env-file central.env -p central up -d --build
 docker compose --env-file old.env -p old up -d --build
 docker compose --env-file new.env -p new up -d --build
